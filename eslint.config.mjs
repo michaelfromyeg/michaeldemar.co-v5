058fcece10b1aae8dbfc05c1e230ad7c4ba67d5a
Michael DeMarco <michaelfromyeg@gmail.com>
2024-12-30 12:49:53 -0700

    feat: drop eslintignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,33 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      '.git/**',
+      '.next/**',
+      'node_modules/**',
+      'public/**',
+      'build/**',
+      'dist/**',
+      'data/*.json',
+    ],
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   ...compat.config({
     extends: ['next', 'prettier'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
     },
   }),
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
