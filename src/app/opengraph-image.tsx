a2a00c439c43be37c5382d9b5b84f6afb4da6f63
Michael DeMarco <michaelfromyeg@gmail.com>
2024-12-31 13:10:02 -0700

    feat: fix opengraph?

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -14,7 +14,7 @@ export default function Image() {
     (
       <div
         style={{
-          background: '#800020',
+          background: '#FFF',
           width: '100%',
           height: '100%',
           display: 'flex',
